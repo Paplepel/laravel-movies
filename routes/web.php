520d0f35ac6f488f0a16d8afa2d4452bbc292acf
Adriaan van Niekerk <adriaan@fgx.co.za>
2022-07-30 13:06:23 +0200

    Admin Crud Panel
    Added auth and check to check if user is an Admin

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,10 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth','adminrole']], function() {
+    Route::get('admin', function () {
+        return view('admin.index');
+    })->name('admin');
+
+});
